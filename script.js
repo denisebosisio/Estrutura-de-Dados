bf739f9cdb78e49b2b4f02eda2c753a946d664a4
denisebosisio <denisebosisio12@gmail.com>
2022-06-11 15:23:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome = "Caroline";
+console.log(nome);
+
+let idade = 22;
+console.log(idade);
+
+let anoAtual = 2022;
+console.log(anoAtual - idade);
+
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
